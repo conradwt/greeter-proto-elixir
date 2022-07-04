74d3f63a336ba4b28671011507a40d73c34dd12a
Conrad Taylor <conradwt@gmail.com>
2022-07-03 22:43:59 -0700

    Rename messages.

--- lib/greeterapi/greeter.pb.ex
+++ lib/greeterapi/greeter.pb.ex
@@ -4,7 +4,7 @@ defmodule Greeterapi.HelloRequest do
 
   field :name, 1, type: :string
 end
-defmodule Greeterapi.HelloReply do
+defmodule Greeterapi.HelloResponse do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
@@ -14,7 +14,7 @@ defmodule Greeterapi.Greeter.Service do
   @moduledoc false
   use GRPC.Service, name: "greeterapi.Greeter", protoc_gen_elixir_version: "0.10.0"
 
-  rpc :SayHello, Greeterapi.HelloRequest, Greeterapi.HelloReply
+  rpc :SayHello, Greeterapi.HelloRequest, Greeterapi.HelloResponse
 end
 
 defmodule Greeterapi.Greeter.Stub do
