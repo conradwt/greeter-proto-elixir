51cc34ba3e680865fbdb37a3010033cd430abd0c
Conrad Taylor <conradwt@gmail.com>
2022-07-05 20:10:52 -0700

    Refresh packages. (#5)

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule GreeterProto.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:grpc, "~> 0.3.1"},
+      {:grpc, github: "elixir-grpc/grpc"},
       {:google_protos, "~> 0.2.0"},
       # 2.9.0 fixes some important bugs, so it's better to use ~> 2.9.0
       {:cowlib, "~> 2.9.0", override: true}
