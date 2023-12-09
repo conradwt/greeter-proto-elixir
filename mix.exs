d9bd993b96616ec744db645d2ebc526bbf829b26
Conrad Taylor <conradwt@gmail.com>
2023-12-09 03:05:40 -0800

    Update system requirements.
    * Update system requirements.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GreeterProto.MixProject do
     [
       app: :greeter_proto,
       version: "0.1.0",
-      elixir: "~> 1.15.4",
+      elixir: "~> 1.15.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
